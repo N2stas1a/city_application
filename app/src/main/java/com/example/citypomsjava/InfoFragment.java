29b6e34019d531bc82a1901cb6175c7e077dbf4f
N2st2s1a <anastasiaborisenko53@gmail.com>
2023-11-14 22:46:25 +0100

    main

---
+++ app/src/main/java/com/example/citypomsjava/InfoFragment.java
@@ -0,0 +1,18 @@
+package com.example.citypomsjava;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
