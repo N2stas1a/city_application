8dfb373f21bd56c3f289597b46ee567a886b2066
N2st2s1a <anastasiaborisenko53@gmail.com>
2023-11-14 23:37:59 +0100

    works

--- app/src/main/java/com/example/citypomsjava/MainActivity.java
+++ app/src/main/java/com/example/citypomsjava/MainActivity.java
@@ -4,10 +4,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-import com.example.bottomnavigation.databinding.ActivityMainBinding;
-import com.example.citypomsjava.DashboardFragment;
-import com.example.citypomsjava.InfoFragment;
-import com.example.citypomsjava.RouteFragment;
+import com.example.citypomsjava.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -24,20 +22,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initBottomNavigationView() {
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.home:
-                    replaceFragment(new HomeFragment());
-                    break;
-                case R.id.shorts:
-                    replaceFragment(new InfoFragment());
-                    break;
-                case R.id.subscriptions:
-                    replaceFragment(new DashboardFragment());
-                    break;
-                case R.id.library:
-                    replaceFragment(new RouteFragment());
-                    break;
+        BottomNavigationView bottomNavigationView = binding.bottomNavigationView;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int selectedItemId = item.getItemId();
+            if (selectedItemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            }
+            else if (selectedItemId == R.id.route) {
+                replaceFragment(new RouteFragment());
+                return true;
+            }
+            else if (selectedItemId == R.id.dashboard) {
+                replaceFragment(new DashboardFragment());
+                return true;
+            }
+            else if (selectedItemId == R.id.info) {
+                replaceFragment(new InfoFragment());
+                return true;
             }
             return true;
         });
