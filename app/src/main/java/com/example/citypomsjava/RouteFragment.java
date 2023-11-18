408930e482d6c61e5dc186158a6ecdbc4bafd55c
N2st2s1a <anastasiaborisenko53@gmail.com>
2023-11-18 18:32:49 +0100

    ккд
    работают 3 элемента навигации из route - трамваи, автобусы и метро.

--- app/src/main/java/com/example/citypomsjava/RouteFragment.java
+++ app/src/main/java/com/example/citypomsjava/RouteFragment.java
@@ -1,22 +1,25 @@
 package com.example.citypomsjava;
 
-import com.example.citypomsjava.AllRouteFragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 public class RouteFragment extends Fragment {
     CardView tramCard;
+    CardView busCard;
+    CardView subwayCard;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_route, container, false);
         tramCard = view.findViewById(R.id.tramCard);
+        busCard = view.findViewById(R.id.busCard);
+        subwayCard = view.findViewById(R.id.subwayCard);
+
         if (tramCard != null) {
             tramCard.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -30,7 +33,37 @@ public class RouteFragment extends Fragment {
                     }
                 }
             });
+            if (busCard != null) {
+                busCard.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (getActivity() != null) {
+                            allSubwayFragment allSubwayFragment = new allSubwayFragment();
+                            getParentFragmentManager().beginTransaction()
+                                    .replace(R.id.frame_layout, allSubwayFragment)
+                                    .addToBackStack(null)
+                                    .commit();
+                        }
+                    }
+                });
+                if (subwayCard != null) {
+                    subwayCard.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (getActivity() != null) {
+                                allSubwayFragment allSubwayFragment = new allSubwayFragment();
+                                getParentFragmentManager().beginTransaction()
+                                        .replace(R.id.frame_layout, allSubwayFragment)
+                                        .addToBackStack(null)
+                                        .commit();
+                            }
+                        }
+                    });
+                }
+            }
         }
         return view;
     }
 }
+
+
---
+++ app/src/main/java/com/example/citypomsjava/allSubwayFragment.java
@@ -0,0 +1,20 @@
+package com.example.citypomsjava;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class allSubwayFragment extends Fragment {
+    public allSubwayFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_subway, container, false);
+    }
+}
